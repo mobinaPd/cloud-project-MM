f2fce0ffb86cb780f53952fea5317e137a2ea8d9
mob <mobnaPooladi97@gmail.com>
2021-11-29 18:56:28 +0330

    queris

--- cloud-project/app/Http/Controllers/GameController.php
+++ cloud-project/app/Http/Controllers/GameController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Game;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class GameController extends Controller
 {
@@ -13,4 +14,39 @@ class GameController extends Controller
     public function rank($rank){
         return Game::where('Rank', $rank)->get();
     }
+
+    public function name($name){
+        return Game::where("Name","LIKE", "%" . $name . "%")->get();
+
+    }
+
+    public function bestGamePlat($N){
+        return DB::table('games')->select('Name')->groupBy('Platform')->orderBy('Rank', 'desc')->limit($N)->get();
+
+    }
+
+    public function bestGameYear($N){
+        return DB::table('games')->select('Name')->groupBy('Year')->orderBy('Rank', 'desc')->limit($N)->get();
+
+    }
+
+    public function bestGameGen($N){
+        return DB::table('games')->select('Name')->groupBy('Genre')->orderBy('Rank', 'desc')->limit($N)->get();
+
+    }
+
+
+    public function best5($year , $plat){
+        return DB::table('games')->select('Name')->groupBy('Platform')->where('Year', '=', $year)->where('Platform', '=', $plat)
+        -> orderBy('Rank', 'desc')->limit(5)->get();
+
+    }
+
+    public function euMoreNa(){
+        return DB::table('games')->select('Name')->where('EU_Sales', '>', 'NA_Sales')->get();
+
+    }
+
 }
+
+
